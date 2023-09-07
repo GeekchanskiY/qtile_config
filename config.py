5b62a0c28454d966d57a1051d943960199666b30
GeekchanskiY <astrovskiy.04@gmail.com>
2023-09-07 23:39:06 +0300

    rofi changed, unsued imports removed, prettify

--- config.py
+++ config.py
@@ -27,15 +27,12 @@
 # import os
 import subprocess
 
-from libqtile import bar, layout, hook, qtile
+from libqtile import bar, layout, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-# from libqtile.utils import guess_terminal
 
 from libqtile.utils import send_notification
 
-# from qtile_extras.widget.decorations import PowerLineDecoration
-# from qtile_extras import widget
 from qtile_extras.popup.toolkit import (
     PopupRelativeLayout,
     PopupImage,
@@ -45,7 +42,6 @@ from qtile_extras.popup.toolkit import (
 from qtile_extras import widget
 
 from modules.docker_widget import DockerWidget
-from modules.VPN_widget import VPNWidget
 
 import logging
 
@@ -143,12 +139,14 @@ keys = [
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
@@ -156,6 +154,7 @@ keys = [
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
@@ -170,9 +169,11 @@ keys = [
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+
+    # Other
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawn('rofi -show run'), desc="Spawn a command using a prompt widget"),
+    Key([mod], "r", lazy.spawn('rofi -show drun'), desc="Spawn a command using a rofi"),
 
     Key([mod,], "space", lazy.widget["keyboardlayout"].next_keyboard(),   desc="Next keyboard layout"),
 
@@ -187,24 +188,18 @@ groups = [Group(i) for i in "123456789"]
 for i in groups:
     keys.extend(
         [
-            # mod1 + letter of group = switch to group
             Key(
                 [mod],
                 i.name,
                 lazy.group[i.name].toscreen(),
                 desc="Switch to group {}".format(i.name),
             ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
             Key(
                 [mod, "shift"],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
                 desc="Switch to & move focused window to group {}".format(i.name),
             ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
 
@@ -217,18 +212,6 @@ layouts = [
         margin=5,
         margin_on_single=5,
     ),
-    # layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
 ]
 
 widget_defaults = dict(
@@ -244,7 +227,6 @@ screens = [
         wallpaper_mode="fill",
         top=bar.Bar(
             [
-                # widget.CurrentLayout(),
                 widget.GroupBox(
 			    highlight_method='block',
 			    rounded=True,
@@ -261,15 +243,8 @@ screens = [
                 widget.Spacer(),
                 widget.Clock(format="%H:%M"),
                 widget.Spacer(),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
-                # widget.Systray(),
-                # widget.Backlight(),
-                # widget.Wlan(),
-                # widget.Bluetooth(),
                 widget.Notify(),
                 DockerWidget(),
-                VPNWidget('Poland'),
                 widget.Net(),
                 keyboard,
                 widget.Volume(
@@ -305,7 +280,6 @@ screens = [
 		    ),
             widget.Spacer(),
             widget.Clock(format='%H:%M'),
-            # AnalogueClock(margin=3, padding=3, second_size=1),
             widget.Spacer()
             ], 
             30,
